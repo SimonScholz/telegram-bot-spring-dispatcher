f5dc17859409918a10ff70eea9a28e62a37ca00b
Simon Scholz <simon.scholz@vogella.com>
2018-03-18 02:28:54 +0100

    Moved HiddenHttpMethodFilter to Config
    Change-Id: I53c07b9d0cf9a85c8d15a018f87093b68860e3d9
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- backend/telegram-bot-dispatcher/src/main/java/de/simonscholz/telegram/controller/Config.java
+++ backend/telegram-bot-dispatcher/src/main/java/de/simonscholz/telegram/controller/Config.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -19,5 +20,10 @@ public class Config {
 	@LoadBalanced
 	public WebClient.Builder getWebClient() {
 		return WebClient.builder();
+	}
+
+	@Bean
+	HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
 	}
 }
